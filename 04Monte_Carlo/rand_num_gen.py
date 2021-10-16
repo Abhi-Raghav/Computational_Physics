e4b122f6eb962b2510ca040a8f8f5bf2b6a9e577
Abhishek <abhishek@jaist.ac.jp>
2021-10-16 17:42:59 +0900

    Added documentation for rand_no_gen

--- 04Monte_Carlo/rand_num_gen.py
+++ 04Monte_Carlo/rand_num_gen.py
@@ -6,14 +6,40 @@ import numpy as np
 
 
 def rand_no_gen(seed, a, c, m, no_of_randnos):
+    """
+    This function generates a numpy array of random numbers (positive integers) using Linear congruent method
+
+    If r_i is initial random number, the next random number in the sequence is given by
+    r_i+1 = (a*r_i + c) % m
+    The random numbers generated will start repeating after m numbers
+    Parameters
+    ----------
+    seed : int
+        The starting 'seed' for the random no. generator
+    a : int
+        The multiplying parameter for the random no. generator
+    c : int
+        The adding parameter for the random no. generator
+    m : int
+        m determines how many number of random nos can be generated. Repetition starts after m entries
+    no_of_randnos : int
+        Total no of random numbers (including seed) required
+
+    Returns
+    -------
+    randno_array : numpy.array(int)
+        A numpy array of the random nos generated
+
+    """
     randno_array = np.zeros(no_of_randnos)
     randno_array[0] = seed
     for i in range(no_of_randnos - 1):
         randno_array[i + 1] = (a * randno_array[i] + c) % m
     return randno_array
 
+
 def main():
-    print(f'## A simple python script to generate random numbers ##')
+    print('## A simple python script to generate random numbers ##')
     no_of_randnos = int(input('Enter the number of random numbers that you want: '))
     seed = int(input('Enter seed value: '))
     a = int(input("Enter 'a' parameter for the generator: "))
