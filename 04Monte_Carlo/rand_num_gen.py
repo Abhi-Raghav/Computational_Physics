d5b7a3a49d5165f8119dec1bb2064123941e6398
Abhishek <abhishek@jaist.ac.jp>
2021-10-16 21:29:27 +0900

    Added a function 'random_generator_test' to plot random numbers split according to odd and even positions. The plot helps in recognizing patterns and correlations in random numbers.

--- 04Monte_Carlo/rand_num_gen.py
+++ 04Monte_Carlo/rand_num_gen.py
@@ -2,7 +2,9 @@
 This is a simple python program to generate pseudo random numbers between 0 and 1 using Linear Congruent method
 """
 
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def rand_no_gen(seed, a, c, m, no_of_randnos):
@@ -27,7 +29,7 @@ def rand_no_gen(seed, a, c, m, no_of_randnos):
 
     Returns
     -------
-    randno_array : numpy.array(int)
+    randno_array : ndarray
         A numpy array of the random nos generated
 
     """
@@ -38,6 +40,29 @@ def rand_no_gen(seed, a, c, m, no_of_randnos):
     return randno_array
 
 
+def random_generator_test(array_of_random_numbers):
+    if len(array_of_random_numbers) % 2 != 0:
+        mod_array = array_of_random_numbers[0:-1]
+    else:
+        mod_array = array_of_random_numbers
+    x_values = []
+    y_values = []
+
+    for i in range(len(mod_array)):
+        if i % 2 == 0:
+            x_values.append(mod_array[i])
+        else:
+            y_values.append(mod_array[i])
+
+    fig, ax = plt.subplots()
+    ax.scatter(x_values, y_values)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('Correlations of random nos')
+    ax.grid(True)
+    plt.show()
+
+
 def main():
     print('## A simple python script to generate random numbers ##')
     no_of_randnos = int(input('Enter the number of random numbers that you want: '))
@@ -45,7 +70,9 @@ def main():
     a = int(input("Enter 'a' parameter for the generator: "))
     c = int(input("Enter 'c' parameter for the generator: "))
     m = int(input("Enter 'm' parameter for the generator: "))
-    print(f'The generated random numbers are {rand_no_gen(seed, a, c, m, no_of_randnos)}')
+    rand_array = rand_no_gen(seed, a, c, m, no_of_randnos)
+    print(f'The generated random numbers are {rand_array}')
+    random_generator_test(rand_array)
 
 
 if __name__ == '__main__':
